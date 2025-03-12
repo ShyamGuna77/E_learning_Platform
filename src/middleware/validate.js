cdf9dfad097c21790a9417deea579504c0771d79
Shyam <gshyamprasad2001@gmail.com>
2025-03-12 22:32:58 +0530

    validation error

---
+++ src/middleware/validate.js
@@ -0,0 +1,28 @@
+
+
+
+const { ZodError } = require("zod");
+
+const validate = (schema) => (req, res, next) => {
+  try {
+    schema.parse({
+      body: req.body,
+      query: req.query,
+      params: req.params,
+    });
+    next();
+  } catch (error) {
+    if (error instanceof ZodError) {
+      return res.status(400).json({
+        success: false,
+        error: error.errors.map((e) => ({
+          path: e.path.join("."),
+          message: e.message,
+        })),
+      });
+    }
+    next(error);
+  }
+};
+
+module.exports = validate;
