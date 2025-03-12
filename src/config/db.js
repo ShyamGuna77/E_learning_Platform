cd4a63d632159c7b5aafec487414b8a623d3b14d
Shyam <gshyamprasad2001@gmail.com>
2025-03-12 14:58:59 +0530

    feat:ADDED

---
+++ src/config/db.js
@@ -0,0 +1,21 @@
+
+const mongoose = require('mongoose')
+
+
+async function ConnectDB() {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("Connected to database");
+    const port = process.env.PORT || 5000;
+    app.listen(port, () => {
+      console.log("Port listening on server 3000");
+    });
+  } catch (error) {
+    console.error(
+      "Failed to connect to the database or start the server",
+      error
+    );
+  }
+}
+
+module.exports ={ConnectDB}
