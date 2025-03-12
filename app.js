b81712f2d6cb0926a7ddf7c8b9e0e9f326bdb43e
Shyam <gshyamprasad2001@gmail.com>
2025-03-12 14:58:47 +0530

    Added Structure of the project

--- app.js
+++ app.js
@@ -3,7 +3,10 @@
 const express = require("express")
 const mongoose = require('mongoose')
 const cors = require("cors")
+const ConnectDB = require('./src/config/db')
 require("dotenv").config();
+
+ConnectDB();
 
 const app = express()
 
@@ -11,23 +14,4 @@ app.use(cors())
 app.use(express.json())
 
 
-async function ConnectDB () {
 
-    try {
-         await mongoose.connect(process.env.MONGO_URL)
-         console.log("Connected to database");
-         const port = process.env.PORT || 5000;  
-         app.listen(port,() => {
-            console.log("Port listening on server 3000");
-         })
-        
-    } catch (error) {
-         console.error(
-           "Failed to connect to the database or start the server",
-           error
-         );
-        
-    }
- 
-}
-ConnectDB()
