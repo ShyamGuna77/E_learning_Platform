fb18e0c3fc8a4def7625073b151a14f9c26f7ace
Shyam <gshyamprasad2001@gmail.com>
2025-03-13 08:10:06 +0530

    feat:added zod validator schema

---
+++ src/validators/authValidator.js
@@ -0,0 +1,23 @@
+
+
+const {z} = require('zod')
+const { password } = require('../models/User')
+
+const registerSchema = z.object({
+    body:z.object({
+        username:z.string().min(4,"Name must be at least 3 characters"),
+        email:z.string().email("Please provide an email"),
+        password:z.string().min(5,"Password must be atleast 5 characters"),
+        role:z.enum(['user','admin']).optional(),
+    })
+})
+
+
+const loginSchema = z.object({
+    body:z.object({
+       email:z.string().email('please provide an email'),
+       password:z.string().min(5,"password is required")
+    })
+});
+
+module.exports ={registerSchema,loginSchema}
